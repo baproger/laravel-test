de12557b51490ae21f77210e9f6a2aee219e11c7
baproger <baproger@MacBook.local>
2024-03-23 00:56:18 +0500

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>Posts</title>
+
+    </head>
+    <body>
+    <header class="p-3 text-bg-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="{{route('posts.index')}}" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="{{route('posts.create')}}" class="nav-link px-2 text-white">Create</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Login</button>
+          <button type="button" class="btn btn-warning">Sign-up</button>
+        </div>
+      </div>
+    </div>
+  </header><br>
+  <div>
+    <a href="{{route('posts.create')}}">Go to Create Page</a>
+  </div>
+
+  
+  @foreach($posts as $post)
+    <a href="{{route('posts.show', $post->id)}}"><h3>{{$post->title}}</h3></a>
+    
+    <p>{{$post->content}}</p>
+
+    <form action="{{route('posts.destroy', $post->id)}}" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    
+    </form>
+
+
+  @endforeach
+    </body>
+</html>
